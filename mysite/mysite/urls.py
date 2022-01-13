c8a783e910ae6bfc516574fe1dd9bb2bc1fc7def
YankaPiskun <yan.piskun.2000@bk.ru>
2022-01-13 19:43:32 +0300

    create list urls for app news

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from news.views import index
+from django.urls import path, include
 from news.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', index),
-    path('test/', test),
+    path('news/', include('news.urls')),
 ]
