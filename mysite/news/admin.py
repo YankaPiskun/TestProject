5cb9ee8c74fc303cceb0eeda4de01ed6ccded0f0
YankaPiskun <yan.piskun.2000@bk.ru>
2022-01-15 01:57:41 +0300

    setting admin panel

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import News
 
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_ad', 'updated_ad', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(News, NewsAdmin)
 # Register your models here.
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=250)
-    content = models.TextField(blank=True) #Не обязательно к заполнению
-    created_ad = models.DateTimeField(auto_now_add=True) #При создании текущая дата устанавлевается
-    updated_ad = models.DateTimeField(auto_now=True) #Устанавливается при каждом редактировании
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=250, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент') #Не обязательно к заполнению
+    created_ad = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации') #При создании текущая дата устанавлевается
+    updated_ad = models.DateTimeField(auto_now=True, verbose_name='Обнавлено') #Устанавливается при каждом редактировании
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликованно')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_ad']
 # Create your models here.
