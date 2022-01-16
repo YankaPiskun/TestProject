486a3e8acb78fe81b16a6b3f6cccb45fda6076c2
YankaPiskun <yan.piskun.2000@bk.ru>
2022-01-16 16:21:53 +0300

    added castom validasion

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Category, News
+import re
+from django.core.exceptions import ValidationError
 
 
 class NewsForm(forms.ModelForm):
@@ -10,6 +12,7 @@ class NewsForm(forms.ModelForm):
     # category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории:',
     # empty_label='Выбериет категорию', widget=forms.Select(attrs={"class": "form-control"}))
     # photo = forms.ImageField(label='Загрузите фото')
+
     class Meta:
         model = News
         fields = '__all__'
@@ -17,8 +20,15 @@ class NewsForm(forms.ModelForm):
             'title': forms.TextInput(attrs={"class": "form-control"}),
             'content': forms.Textarea(attrs={"class": "form-control", 'rows': 5}),
             'category': forms.Select(attrs={"class": "form-control"}),
-            'title': forms.TextInput(attrs={"class": "form-control"}),
         }
+
+        def clean_title(self):
+            title = self.cleaned_data['title']
+            if re.match(r'\d', title):
+                raise ValidationError('Название не должно начинаться с цифры')
+            return title
+
+
 
 
 
